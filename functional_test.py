a1d5341e0b07b46282be876154377cf5c0a5d000
Gustav Danielsson <gdanielsson@me.com>
2018-06-09 22:20:25 -0400

    update functional test to test for adding a todo.

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -14,12 +16,36 @@ class NewVisitorTest(unittest.TestCase):
         # User can go to page
         self.browser.get('http://localhost:8000')
 
-        # sees the title mentions to-dos
+        # sees the title and header mentions to-dos
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
         self.fail('Finish test later')
 
-        # User can enter new todos
+        # User can enter new todos with a input that has a placeholder
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # User enters Get a haircut as a to do
+        inputbox.send_keys('Get a haircut')
+
+        # User hits enter
+        inputbox.send_keys(Keys.ENTER)  
+        time.sleep(1)
+
+        # User sees "1: Get a haircut" in a todos table
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')  
+        self.assertTrue(
+            any(row.text == '1: Get a haircut' for row in rows)
+        )
+
+        self.fail('Finish the test!')
         # User can come back to page and see todos
+        
 
 
 if __name__ == '__main__':
