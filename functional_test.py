411342765ca98d5f0ff41ba3a7e61dee73e343cc
Gustav Danielsson <gdanielsson@me.com>
2018-06-09 22:41:22 -0400

    add input and table to home page.

--- functional_test.py
+++ functional_test.py
@@ -20,7 +20,6 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text  
         self.assertIn('To-Do', header_text)
-        self.fail('Finish test later')
 
         # User can enter new todos with a input that has a placeholder
         inputbox = self.browser.find_element_by_id('id_new_item')  
@@ -40,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')  
         self.assertTrue(
-            any(row.text == '1: Get a haircut' for row in rows)
+            any(row.text == '1: Get a haircut' for row in rows),
+            'New to-do item did not appear in table'
         )
 
         self.fail('Finish the test!')
