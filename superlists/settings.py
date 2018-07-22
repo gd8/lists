91277d9496c7e1cfb82c50e87f61e6ab682e0451
Gustav Danielsson <gdanielsson@me.com>
2018-07-22 10:03:44 -0400

    add logic for prod/dev settings: secret key, debug, allowed hosts.

--- superlists/settings.py
+++ superlists/settings.py
@@ -16,16 +16,14 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8fj$u%2s@sen#%)njmdq2sn^6klus(uq%(iw+_4h596ywq6om_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    SECRET_KEY = '8fj$u%2s@sen#%)njmdq2sn^6klus(uq%(iw+_4h596ywq6om_'
+    DEBUG = True
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
